5c25878dc8fd62656ab1ef92fa558008605d578b
SpaceEstate <patrick.comper@yahoo.com>
2025-11-06 16:20:34 +0100

    Add customer email to Stripe payment session

--- api/crea-pagamento-stripe.js
+++ api/crea-pagamento-stripe.js
@@ -124,6 +124,7 @@ export default async function handler(req, res) {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
+      customer_email: responsabile.email || undefined,
       line_items: [
         {
           price_data: {
